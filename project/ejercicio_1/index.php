b354aec33eb32cdf5e8e97257588ff76f7759a20
Diego Forero <gollum23@gmail.com>
2016-09-21 22:48:28 -0500

    First Commit

---
+++ project/ejercicio_1/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Diego Forero</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+    <link href="http://alexgorbatchev.com/pub/sh/current/styles/shCore.css" rel="stylesheet" type="text/css" />
+    <link href="http://alexgorbatchev.com/pub/sh/current/styles/shThemeDefault.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="container">
+    <header>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="page-header">
+                    <h1 class="text-center">Programación de Sitios Web</h1>
+                    <h3 class="text-center">Ejercicio 1</h3>
+                    <div class="text-center">
+                        <a href="/">Volver al menu</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <div class="content">
+
+        <form action="." method="post">
+            <div class="row">
+                <div class="col-xs-4 col-xs-offset-4">
+                    <div class="form-group">
+                        <label for="number_one" >Primer número</label>
+                        <input class="form-control" type="text" id="number_one" name="number_one">
+                    </div>
+                    <div class="form-group">
+                        <label for="number_two">Segundo número</label>
+                        <input class="form-control" type="text" id="number_two" name="number_two">
+                    </div>
+                    <button type="submit" class="btn btn-success">Calcular!</button>
+                </div>
+            </div>
+        </form>
+
+        <?php
+            if (isset($_POST['number_one']) and isset($_POST['number_two'])) {
+                $number_one = $_POST['number_one'];
+                $number_two = $_POST['number_two'];
+
+                $not_divisor = $number_two % $number_one;
+
+                $message = $number_one . " es divisor de " . $number_two;
+
+                if($not_divisor) {
+                    $message = $number_one . " no es divisor de " . $number_two;
+                }
+        ?>
+                <br>
+                <div class="row show-grid">
+                    <div class="col-xs-4 col-xs-offset-4">
+                        <div class="panel panel-success">
+                            <div class="panel-heading">
+                                Respuesta
+                            </div>
+                            <div class="panel-body">
+                                <?=$message;?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        <?php
+            }
+        ?>
+
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+                <h3 class="text-center">Código del ejercicio</h3>
+                <pre class="brush: php">
+    &ltdiv class="content"&gt
+
+        &ltform action="." method="post"&gt
+            &ltdiv class="row"&gt
+                &ltdiv class="col-xs-4 col-xs-offset-4"&gt
+                    &ltdiv class="form-group"&gt
+                        &ltlabel for="number_one" &gtPrimer número&lt/label&gt
+                        &ltinput class="form-control" type="text" id="number_one" name="number_one"&gt
+                    &lt/div&gt
+                    &ltdiv class="form-group"&gt
+                        &ltlabel for="number_two"&gtSegundo número&lt/label&gt
+                        &ltinput class="form-control" type="text" id="number_two" name="number_two"&gt
+                    &lt/div&gt
+                    &ltbutton type="submit" class="btn btn-success"&gtCalcular!&lt/button&gt
+                &lt/div&gt
+            &lt/div&gt
+        &lt/form&gt
+
+        &lt?php
+        if (isset($_POST['number_one']) and isset($_POST['number_two'])) {
+            $number_one = $_POST['number_one'];
+            $number_two = $_POST['number_two'];
+
+            $not_divisor = $number_two % $number_one;
+
+            $message = $number_one . " es divisor de " . $number_two;
+
+            if($not_divisor) {
+                $message = $number_one . " no es divisor de " . $number_two;
+            }
+            ?&gt
+            &ltbr&gt
+            &ltdiv class="row show-grid"&gt
+                &ltdiv class="col-xs-4 col-xs-offset-4"&gt
+                    &ltdiv class="panel panel-success"&gt
+                        &ltdiv class="panel-heading"&gt
+                            Respuesta
+                        &lt/div&gt
+                        &ltdiv class="panel-body"&gt
+                            &lt?=$message;?&gt
+                        &lt/div&gt
+                    &lt/div&gt
+                &lt/div&gt
+            &lt/div&gt
+            &lt?php
+        }
+        ?&gt
+    &lt/div&gt
+                </pre>
+            </div>
+        </div>
+
+
+    </div>
+
+    <footer>
+        <div class="row">
+            <div class="col-xs-12">
+                <h2 class="text-center">Universidad Nacional Abierta y a Distancia</h2>
+                <h4 class="text-center">Grupo 301127 - 13</h4>
+                <p class="text-center">Bogotá, Colombia Octubre 2016</p>
+            </div>
+        </div>
+    </footer>
+</div>
+
+
+<script src="../js/bootstrap.min.js"></script>
+<script src="http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js" type="text/javascript"></script>
+<script src="http://alexgorbatchev.com/pub/sh/current/scripts/shAutoloader.js" type="text/javascript"></script>
+<script src="http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPhp.js" type="text/javascript"></script>
+<script>
+    SyntaxHighlighter.all();
+</script>
+</body>
+</html>
+<?php
+
---
+++ project/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Diego Forero</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="page-header">
+                            <h1 class="text-center">Programación de Sitios Web</h1>
+                            <h4 class="text-center">Diego Alexander Forero Higuera</h4>
+                        </div>
+                    </div>
+                </div>
+            </header>
+
+            <div class="content">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 1</div>
+                    <div class="panel-body">
+                        <p>Capturar dos números e indicar si el primer número es divisor del segundo número</p>
+                        <p><a href="ejercicio_1/">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 2</div>
+                    <div class="panel-body">
+                        <p>Por medio de un formulario, pedir la base y el exponente, y realizar el cálculo mediante sumas</p>
+                        <p><a href="#">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 3</div>
+                    <div class="panel-body">
+                        <p>Mediante un formulario pedir un numero inicial y un número final, e indicar de estos números los que
+                            son primos y cuantos no son primos</p>
+                        <p><a href="#">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 4</div>
+                    <div class="panel-body">
+                        <p>Un script debe pedirle al usuario que digite la cantidad de números a generar, luego se debe crear la
+                            siguiente secuencia (1, 5, 6, 10, 11, 15, 16, …)</p>
+                        <p><a href="#">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">Ejercicio 5</div>
+                    <div class="panel-body">
+                        <p>Diseñe un programa que permita ingresar la edad del usuario y otro número cualquiera (a).</p>
+                        <p>El programa sumará el número “a” a la edad ingresada y generará la tabla de multiplicar de este número</p>
+                        <p><a href="#">Ir al ejercicio</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h2 class="text-center">Universidad Nacional Abierta y a Distancia</h2>
+                        <h4 class="text-center">Grupo 301127 - 13</h4>
+                        <p class="text-center">Bogotá, Colombia Octubre 2016</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+
+        <script src="./js/bootstrap.min.js"></script>
+    </body>
+</html>
